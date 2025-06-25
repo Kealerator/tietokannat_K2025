211fe7c37fde25858049109dfad4018d1f298a78
Kealerator <matti.rundelin@gmail.com>
2025-06-25 20:57:35 +0300

    Dataa lisätty.
    dump päivitetty.

--- Kirjastotietokanta/MySQL_dumps/dump_Kirjastotietokanta.sql
+++ Kirjastotietokanta/MySQL_dumps/dump_Kirjastotietokanta.sql
@@ -24,10 +24,10 @@ DROP TABLE IF EXISTS `Asiakas`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `Asiakas` (
   `idAsiakas` int NOT NULL AUTO_INCREMENT,
-  `Etunimi` varchar(45) DEFAULT NULL,
-  `Sukunimi` varchar(45) DEFAULT NULL,
-  `Osoite` varchar(90) DEFAULT NULL,
-  `Sähköposti` varchar(90) DEFAULT NULL,
+  `Etunimi` varchar(45) NOT NULL,
+  `Sukunimi` varchar(45) NOT NULL,
+  `Osoite` varchar(90) NOT NULL,
+  `Sähköposti` varchar(90) NOT NULL,
   `Korttinumero` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`idAsiakas`),
   UNIQUE KEY `Korttinumero_UNIQUE` (`Korttinumero`)
@@ -107,7 +107,7 @@ CREATE TABLE `Kirja` (
   KEY `fk_Kirja_Hylly1_idx` (`idHylly`,`idOsasto`),
   CONSTRAINT `fk_Kirja_Hylly1` FOREIGN KEY (`idHylly`, `idOsasto`) REFERENCES `Hylly` (`idHylly`, `idOsasto`),
   CONSTRAINT `fk_Kirja_Teos1` FOREIGN KEY (`idTeos`) REFERENCES `Teos` (`idTeos`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -116,7 +116,7 @@ CREATE TABLE `Kirja` (
 
 LOCK TABLES `Kirja` WRITE;
 /*!40000 ALTER TABLE `Kirja` DISABLE KEYS */;
-INSERT INTO `Kirja` VALUES (1,2,'Fantasia','Englanti','2012-09-01','ISBN9780547928241',1,4),(2,3,'Fantasia','Englanti','1991-07-04','ISBN9780261102354',1,4),(4,4,'Lasten & Nuorten kaunokirjallisuus','Suomi','2024-08-09','ISBN9789522563477',1,13);
+INSERT INTO `Kirja` VALUES (1,2,'Fantasia','Englanti','2012-09-01','ISBN9780547928241',1,4),(2,3,'Fantasia','Englanti','1991-07-04','ISBN9780261102354',1,4),(4,4,'Lasten & Nuorten kaunokirjallisuus','Suomi','2024-08-09','ISBN9789522563477',1,13),(5,5,'Fantasia','Englanti','2007-04-01','ISBN9780261102361',1,4);
 /*!40000 ALTER TABLE `Kirja` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -221,7 +221,7 @@ CREATE TABLE `Tekijä_has_Teos` (
 
 LOCK TABLES `Tekijä_has_Teos` WRITE;
 /*!40000 ALTER TABLE `Tekijä_has_Teos` DISABLE KEYS */;
-INSERT INTO `Tekijä_has_Teos` VALUES (1,2),(1,3),(2,4),(3,4);
+INSERT INTO `Tekijä_has_Teos` VALUES (1,2),(1,3),(2,4),(3,4),(1,5);
 /*!40000 ALTER TABLE `Tekijä_has_Teos` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -237,7 +237,7 @@ CREATE TABLE `Teos` (
   `Nimi` varchar(90) NOT NULL,
   `Julkaisuvuosi` year DEFAULT NULL,
   PRIMARY KEY (`idTeos`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -246,7 +246,7 @@ CREATE TABLE `Teos` (
 
 LOCK TABLES `Teos` WRITE;
 /*!40000 ALTER TABLE `Teos` DISABLE KEYS */;
-INSERT INTO `Teos` VALUES (2,'The Hobbit, or There and Back Again',1937),(3,'The Fellowship of The Ring',1954),(4,'Lasten toivesatukirja',2024);
+INSERT INTO `Teos` VALUES (2,'The Hobbit, or There and Back Again',1937),(3,'The Fellowship of The Ring',1954),(4,'Lasten toivesatukirja',2024),(5,'The Two Towers',1954);
 /*!40000 ALTER TABLE `Teos` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -277,4 +277,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-06-24 22:47:01
+-- Dump completed on 2025-06-25 20:57:28
